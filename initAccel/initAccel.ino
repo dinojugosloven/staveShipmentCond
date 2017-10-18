04a58b5b38546e32837edae0713eba4ec425c19d
Dino Tahirovic <e.tahirovic@qmul.ac.uk>
2017-10-18 19:43:54 +0100

    Corrected last commit. Works stable at 500 Hz. Output every 1 s not to overflow USB port.

--- initAccel/initAccel.ino
+++ initAccel/initAccel.ino
@@ -6,12 +6,16 @@
 #include <Wire.h>
 #include <Arduino.h>
 extern "C" {
+#include "util/arduino_mpu9250_i2c.h"
 #include "util/inv_mpu.h"
 #include "util/inv_mpu_dmp_motion_driver.h"
 }
 #include <MPU9250_RegisterMap.h>
 
+#define MPU9250_ADDR 0x68
 #define com SerialUSB
+#define i2c_read arduino_i2c_read
+#define i2c_write arduino_i2c_write
 
 // global types & constants
 #define SD_LOG_WRITE_BUFFER_SIZE 64
@@ -29,32 +33,56 @@ enum t_axisOrder {
 
 // Global variables
 //String measurement = "";
-unsigned long accelBuffer[BUFFER_SIZE];
+struct Acceleration{
+  /* Raw data */
+  short ax;
+  short ay;
+  short az;
+  /* time in ms from MPU9250 */
+  unsigned long t;};
+Acceleration accelBuffer[BUFFER_SIZE];
+
+unsigned short _aSense;
+
+unsigned long timeStamp, previousTime;
 unsigned short dataPointer; // how large is short - 8 bits? only 256 then
 
-void setup() 
-{
-  com.begin(115200);
-
-  dataPointer = 0;
-
-  // MPU::Begin
-  inv_error_t result;
-  struct int_param_s int_param;
-  Wire.begin(); //communication between ARM Cortex and MPU9250?
-  result = mpu_init(&int_param);
-
-  if (result != INV_SUCCESS)
+void fail(void)
     { 
       com.println("MPU9520 Accelerator not available");
       com.println("Ctrl+Z should finish the program.");
       while(1);
-    }
+    };
+    
+void setup() 
+{
+  com.begin(115200);
+
+// Initialize global vars
+  dataPointer = 0;
+  previousTime = timeStamp = millis();
+
+  // MPU::Begin
+  inv_error_t result;
+  struct int_param_s int_param;
+  Wire.begin(); //communication between ARM Cortex and MPU9250
+  result = mpu_init(&int_param);
+
+  if (result != INV_SUCCESS) fail();
+
+  
   // Init accelerometer
   mpu_set_bypass(1); // Place all slaves (including compass) on primary bus
-  mpu_set_sensors(INV_XYZ_ACCEL);
-  mpu_set_sample_rate(1000);
-  delay(3000);
+  mpu_set_sensors(INV_XYZ_ACCEL | INV_XYZ_GYRO); // to keep PLL clock
+  mpu_set_sample_rate(500);
+  // set and check full scale range
+  mpu_set_accel_fsr(16);
+  if (int st = mpu_get_accel_sens(&_aSense)) fail();
+  // Set interrupt enable when raw data ready
+  unsigned char bit1 = 0x1;
+  i2c_write(MPU9250_ADDR, MPU9250_INT_ENABLE, 1, &bit1);
+  
+  delay(2000);
 
   // Probe the sensor
   unsigned char c;
@@ -62,49 +90,64 @@ void setup()
   
   com.print("MPU9520 Who am I returns 0x"); com.println(c, HEX);
 
-  if (c == 0x71) // WHO_AM_I should always be 0x71 for MPU9250
+  // Check the configuration and print values
+  if (c == 0x71) // WHO_AM_I returns 0x71 for MPU9250
   {
     com.println("MPU9250 is online...");
-    
-    //mpu.setSampleRate(500);
-    //unsigned short rate = mpu.getSampleRate();
-    //com.println("Sample rate obtained: ");
-    //com.println(rate);
-    
-    //com.print("Accel sensitivity, LSB value 1/"); com.println(mpu.getASense());
-    //mpu.setAccelFSR(16);
-    //com.print("Accel FSR "); com.println(mpu.getAccelFSR());
 
-    com.println("Reg (HEX)   Value (BIN)");
-    // General configuration
+    com.println("Reg (HEX)       Value");
+    // Sample rate
     unsigned char registerByte;
-    mpu_read_reg(MPU9250_CONFIG, &registerByte);
-    com.print(MPU9250_CONFIG,HEX);com.print("         ");com.println(registerByte,BIN);
-
-    //Accelerometer configuration
-    mpu_set_accel_fsr(16);
-    unsigned char microReg = MPU9250_ACCEL_CONFIG;
+    unsigned char microReg = MPU9250_SMPLRT_DIV;
     mpu_read_reg(microReg, &registerByte);
-    com.print("FSR [g]");com.print("        ");com.println(registerByte,DEC);
+    com.print(microReg,HEX);com.print(" SMPLRT_DIV ");com.println(registerByte,BIN);
+    
+    // General configuration
+    mpu_read_reg(MPU9250_CONFIG, &registerByte);
+    com.print(MPU9250_CONFIG,HEX);com.print("          ");com.println(registerByte,BIN);
 
+    //Accelerometer configuration 1
+    microReg = MPU9250_ACCEL_CONFIG;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX); com.print(" FS_SEL     ");
+    com.println((registerByte >> ACCEL_CONFIG_ACCEL_FS_SEL)&MPU9250_ACCEL_FS_SEL_MASK,BIN);
+
+    //Digital filter and ?? accel_fchoice_b
     microReg = MPU9250_ACCEL_CONFIG_2;
     mpu_read_reg(microReg, &registerByte);
     com.print(microReg,HEX);com.print("        ");com.println(registerByte,BIN);
-
+  
     // I2C master clock frequency
     microReg = MPU9250_I2C_MST_CTRL;
     mpu_read_reg(microReg, &registerByte);
-    com.print(microReg,HEX);com.print("        ");com.println(registerByte,BIN);
+    com.print(microReg,HEX);com.print(" I2C MST CTRL ");com.println(registerByte,BIN);
+    
+    // Interrupt setting
+    microReg = MPU9250_INT_ENABLE;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" INT_EN    ");com.println(registerByte,BIN);
+
+    microReg = MPU9250_INT_STATUS;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" INT_ST    ");com.println(registerByte,BIN);
     
     // Power managment 1
-     microReg = MPU9250_PWR_MGMT_1;
+    microReg = MPU9250_PWR_MGMT_1;
     mpu_read_reg(microReg, &registerByte);
-    com.print(microReg,HEX);com.print("        ");com.println(registerByte,BIN);
+    com.print(microReg,HEX);com.print(" PWR_1 1-5 are PLL CLK ");com.println(registerByte&7);
 
     // Power management 2
     microReg = MPU9250_PWR_MGMT_2;
     mpu_read_reg(microReg, &registerByte);
-    com.print(microReg,HEX);com.print("        ");com.println(registerByte,BIN);
+    com.print(microReg,HEX);com.print(" PWR_2     ");com.println(registerByte,BIN);
+
+    long aBias[3];
+    mpu_read_6500_accel_bias(aBias);
+    com.print("Accel offsets: aX "); com.print(aBias[0]);
+    com.print(" aY "); com.print(aBias[1]);
+    com.print(" aZ "); com.println(aBias[2]);
+
+    com.print(" Accel sensitivity "); com.println(_aSense);
   
   } // MPU9250 check loop
 
@@ -113,28 +156,40 @@ void setup()
 void loop() {
 
   unsigned char intStatusReg;
-  if (mpu_read_reg(MPU9250_INT_STATUS, &intStatusReg) == INV_SUCCESS)
+  mpu_read_reg(MPU9250_INT_STATUS, &intStatusReg);
+  
+  //unsigned long current = micros();
+  if (intStatusReg & (1<<INT_STATUS_RAW_DATA_RDY_INT))
   {
-    unsigned int statusReady = (intStatusReg & (1<<INT_STATUS_RAW_DATA_RDY_INT));
+    //unsigned int statusReady = (intStatusReg & (1<<INT_STATUS_RAW_DATA_RDY_INT));
     
-    // read accel data
+    // read accel data directly from MPU9250 register
     short data[3];
-    unsigned long timeStamp;
-    
-    if ( !mpu_get_accel_reg(data, &timeStamp) )
+    timeStamp = millis();
+    unsigned char tmp[6];
+    if (!i2c_read(MPU9250_ADDR, MPU9250_ACCEL_XOUT_H, 6, tmp))
     {
-      //com.print(micros()/1000.0); com.print(" ");com.print(timeStamp); com.println(" "); 
-      //com.print(data[X_AXIS]); com.print(" "); com.print(data[Y_AXIS]); com.print(" "); com.println(data[Z_AXIS]);
-      accelBuffer[dataPointer] = timeStamp;
+      accelBuffer[dataPointer].ax = (tmp[0] << 8) | tmp[1];
+      accelBuffer[dataPointer].ay = (tmp[2] << 8) | tmp[3];
+      accelBuffer[dataPointer].az = (tmp[4] << 8) | tmp[5];
+      accelBuffer[dataPointer].t = timeStamp;
       dataPointer++;
     }
   } // INT_STATUS ready
+  //com.println(micros()-current);
   
   if (dataPointer == MAX_BUFFER_LENGTH) {
     //buffer full
-    for (int i=0; i<MAX_BUFFER_LENGTH; i++) {
-      com.print(i);com.print(" ");
-      com.print(accelBuffer[i]); com.println(" ms");
+    // Do not overload the USB port, print out every second
+    if ((timeStamp-previousTime) >> 10){
+      for (int i=0; i<MAX_BUFFER_LENGTH; i++) {
+        com.print(i);com.print(" ");
+        com.print(accelBuffer[i].ax/_aSense,DEC); com.print(" "); 
+        com.print(accelBuffer[i].ay/_aSense,DEC); com.print(" ");
+        com.print(accelBuffer[i].az/_aSense,DEC); com.print(" ");
+        com.print(accelBuffer[i].t); com.println(" ms");
+      }
+      previousTime = timeStamp;
     }
     dataPointer = 0;
   }
