257be48e6fc1af7d124fb2354dbc25dd18aa38d0
Dino Tahirovic <e.tahirovic@qmul.ac.uk>
2017-10-31 13:43:20 +0000

    Removing obsolete files. Added new folder accelSDcard, C code with SD card logging.

---
+++ accelSDcard/accelSDcard.ino
@@ -0,0 +1,295 @@
+/** Dino Tahirovic, QMUL
+ *  30/10/2017
+ */
+#include "config.h"
+ 
+#include <Wire.h>
+#include <Arduino.h>
+extern "C" {
+#include "util/arduino_mpu9250_i2c.h"
+#include "util/inv_mpu.h"
+#include "util/inv_mpu_dmp_motion_driver.h"
+}
+#include <MPU9250_RegisterMap.h>
+
+#define MPU9250_ADDR 0x68
+#define com SerialUSB
+#define i2c_read arduino_i2c_read
+#define i2c_write arduino_i2c_write
+
+// global types & constants
+#define SD_LOG_WRITE_BUFFER_SIZE 64
+#define BUFFER_SIZE 64
+#define MAX_BUFFER_LENGTH 63
+
+typedef int inv_error_t;
+#define INV_SUCCESS 0
+#define INV_ERROR 0x20
+enum t_axisOrder {
+  X_AXIS, // 0
+  Y_AXIS, // 1
+  Z_AXIS  // 2
+};
+
+// Global variables
+//String measurement = "";
+struct Acceleration{
+  /* Raw data */
+  short ax;
+  short ay;
+  short az;
+  /* time in ms from MPU9250 */
+  unsigned long t;};
+Acceleration accelBuffer[BUFFER_SIZE];
+
+unsigned short _aSense;
+
+unsigned long timeStamp, previousTime;
+unsigned short dataPointer; // how large is short - 8 bits? only 256 then
+
+/////////////////////
+// SD Card Globals //
+/////////////////////
+bool sdCardPresent = false; // Keeps track of if SD card is plugged in
+String logFileName; // Active logging file
+String logFileBuffer; // Buffer for logged data. Max is set in config
+
+void fail(void)
+    { 
+      com.println("MPU9520 Accelerator not available");
+      com.println("Ctrl+Z should finish the program.");
+      while(1);
+    };
+
+void setup() {
+
+  com.begin(115200);
+
+// Initialize global vars
+  dataPointer = 0;
+  previousTime = timeStamp = millis();
+
+  // MPU::Begin
+  inv_error_t result;
+  struct int_param_s int_param;
+  Wire.begin(); //communication between ARM Cortex and MPU9250
+  result = mpu_init(&int_param);
+
+  if (result != INV_SUCCESS) fail();
+
+  
+  // Init accelerometer
+  mpu_set_bypass(1); // Place all slaves (including compass) on primary bus
+  mpu_set_sensors(INV_XYZ_ACCEL | INV_XYZ_GYRO); // to keep PLL clock
+  mpu_set_sample_rate(500);
+  // set and check full scale range
+  mpu_set_accel_fsr(16);
+  if (int st = mpu_get_accel_sens(&_aSense)) fail();
+  // Set interrupt enable when raw data ready
+  unsigned char bit1 = 0x1;
+  i2c_write(MPU9250_ADDR, MPU9250_INT_ENABLE, 1, &bit1);
+  
+  delay(2000);
+
+  // Probe the sensor
+  unsigned char c;
+  mpu_read_reg(MPU9250_WHO_AM_I, &c);
+  
+  com.print("MPU9520 Who am I returns 0x"); com.println(c, HEX);
+
+  // Check the configuration and print values
+  if (c == 0x71) // WHO_AM_I returns 0x71 for MPU9250
+  {
+    com.println("MPU9250 is online...");
+
+    com.println("Reg (HEX)       Value");
+    // Sample rate
+    unsigned char registerByte;
+    unsigned char microReg = MPU9250_SMPLRT_DIV;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" SMPLRT_DIV ");com.println(registerByte,BIN);
+    
+    // General configuration
+    mpu_read_reg(MPU9250_CONFIG, &registerByte);
+    com.print(MPU9250_CONFIG,HEX);com.print("          ");com.println(registerByte,BIN);
+
+    //Accelerometer configuration 1
+    microReg = MPU9250_ACCEL_CONFIG;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX); com.print(" FS_SEL     ");
+    com.println((registerByte >> ACCEL_CONFIG_ACCEL_FS_SEL)&MPU9250_ACCEL_FS_SEL_MASK,BIN);
+
+    //Digital filter and ?? accel_fchoice_b
+    microReg = MPU9250_ACCEL_CONFIG_2;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print("        ");com.println(registerByte,BIN);
+  
+    // I2C master clock frequency
+    microReg = MPU9250_I2C_MST_CTRL;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" I2C MST CTRL ");com.println(registerByte,BIN);
+    
+    // Interrupt setting
+    microReg = MPU9250_INT_ENABLE;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" INT_EN    ");com.println(registerByte,BIN);
+
+    microReg = MPU9250_INT_STATUS;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" INT_ST    ");com.println(registerByte,BIN);
+    
+    // Power managment 1
+    microReg = MPU9250_PWR_MGMT_1;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" PWR_1 1-5 are PLL CLK ");com.println(registerByte&7);
+
+    // Power management 2
+    microReg = MPU9250_PWR_MGMT_2;
+    mpu_read_reg(microReg, &registerByte);
+    com.print(microReg,HEX);com.print(" PWR_2     ");com.println(registerByte,BIN);
+
+    long aBias[3];
+    mpu_read_6500_accel_bias(aBias);
+    com.print("Accel offsets: aX "); com.print(aBias[0]);
+    com.print(" aY "); com.print(aBias[1]);
+    com.print(" aZ "); com.println(aBias[2]);
+
+    com.print(" Accel sensitivity "); com.println(_aSense);
+  
+  } // MPU9250 check loop
+
+  // Check for the presence of an SD card, and initialize it:
+  if ( initSD() )
+  {
+    sdCardPresent = true;
+    // Get the next, available log file name
+    logFileName = nextLogFile(); 
+  }
+}
+
+void loop() {
+  
+  unsigned char intStatusReg;
+  mpu_read_reg(MPU9250_INT_STATUS, &intStatusReg);
+  
+  //unsigned long current = micros();
+  if (intStatusReg & (1<<INT_STATUS_RAW_DATA_RDY_INT))
+  {
+    //unsigned int statusReady = (intStatusReg & (1<<INT_STATUS_RAW_DATA_RDY_INT));
+    
+    // read accel data directly from MPU9250 register
+    short data[3];
+    timeStamp = millis();
+    unsigned char tmp[6];
+    if (!i2c_read(MPU9250_ADDR, MPU9250_ACCEL_XOUT_H, 6, tmp))
+    {
+      accelBuffer[dataPointer].ax = (tmp[0] << 8) | tmp[1];
+      accelBuffer[dataPointer].ay = (tmp[2] << 8) | tmp[3];
+      accelBuffer[dataPointer].az = (tmp[4] << 8) | tmp[5];
+      accelBuffer[dataPointer].t = timeStamp;
+      dataPointer++;
+    }
+  } // INT_STATUS ready
+  //com.println(micros()-current);
+  
+  if (dataPointer == MAX_BUFFER_LENGTH) {
+    //buffer full
+    // Do not overload the USB port, print out every second
+    if ((timeStamp-previousTime) >> 10){
+      for (int i=0; i<MAX_BUFFER_LENGTH; i++) {
+        com.print(i);com.print(" ");
+        com.print(accelBuffer[i].ax/_aSense,DEC); com.print(" "); 
+        com.print(accelBuffer[i].ay/_aSense,DEC); com.print(" ");
+        com.print(accelBuffer[i].az/_aSense,DEC); com.print(" ");
+        com.print(accelBuffer[i].t); com.println(" ms");
+      }
+      previousTime = timeStamp;
+    }
+    dataPointer = 0;
+  }
+
+   // If SD card logging is enabled & a card is plugged in
+  if ( sdCardPresent)
+  {
+    // If adding this log line will put us over the buffer length:
+    if (imuLog.length() + logFileBuffer.length() >=
+        SD_LOG_WRITE_BUFFER_SIZE)
+    {
+      sdLogString(logFileBuffer); // Log SD buffer
+      logFileBuffer = ""; // Clear SD log buffer 
+      blinkLED(); // Blink LED every time a new buffer is logged to SD
+    }
+    // Add new line to SD log buffer
+    logFileBuffer += imuLog;
+  }
+  else
+  {
+    // Blink LED once every second (if only logging to serial port)
+    if ( millis() > lastBlink + UART_BLINK_RATE )
+
+}
+
+bool initSD(void)
+{
+  // SD.begin should return true if a valid SD card is present
+  if ( !SD.begin(SD_CHIP_SELECT_PIN) )
+  {
+    SerialPort.println("SD card not present.");
+    return false;
+  }
+  
+  SerialPort.println("SD card present.");
+  return true;
+}
+
+// Log a string to the SD card
+bool sdLogString(String toLog)
+{
+  // Open the current file name:
+  File logFile = SD.open(logFileName, FILE_WRITE);
+  
+  // If the file will get too big with this new string, create
+  // a new one, and open it.
+  if (logFile.size() > (SD_MAX_FILE_SIZE - toLog.length()))
+  {
+    logFileName = nextLogFile();
+    logFile = SD.open(logFileName, FILE_WRITE);
+  }
+
+  // If the log file opened properly, add the string to it.
+  if (logFile)
+  {
+    logFile.print(toLog);
+    logFile.close();
+
+    return true; // Return success
+  }
+
+  return false; // Return fail
+}
+
+// Find the next available log file. Or return a null string
+// if we've reached the maximum file limit.
+String nextLogFile(void)
+{
+  String filename;
+  int logIndex = 0;
+
+  for (int i = 0; i < LOG_FILE_INDEX_MAX; i++)
+  {
+    // Construct a file with PREFIX[Index].SUFFIX
+    filename = String(LOG_FILE_PREFIX);
+    filename += String(logIndex);
+    filename += ".";
+    filename += String(LOG_FILE_SUFFIX);
+    // If the file name doesn't exist, return it
+    if (!SD.exists(filename))
+    {
+      return filename;
+    }
+    // Otherwise increment the index, and try again
+    logIndex++;
+  }
+
+  return "";
+}
